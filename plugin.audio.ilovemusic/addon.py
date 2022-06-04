21e16349cd09513d8d97541a88c6623b87c7b1b2
adiko01 <adiko01@adiko01-Fujitsu-M>
2022-06-04 12:56:06 +0200

    V3.0.0.1 - TESTVERSION Beim öffnen des Addons wird die Senderliste neu geladen.

--- plugin.audio.ilovemusic/addon.py
+++ plugin.audio.ilovemusic/addon.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import xbmcplugin,xbmcaddon,xbmcgui,os,sys
+import xbmcplugin,xbmcaddon,xbmcgui,os,sys,requests
 
 addon = xbmcaddon.Addon()
 addon_path = addon.getAddonInfo('path').decode('utf-8')
@@ -15,9 +15,18 @@ def add_item(url,infolabels,img=''):
     listitem.setProperty('IsPlayable','true')
     xbmcplugin.addDirectoryItem(int(sys.argv[1]),url,listitem)    
 
+def sender_update():
+    addon_path = addon.getAddonInfo('path').decode('utf-8')
+    icons_path = os.path.join(addon_path,'resources','senderlogos')
+    senderliste = os.path.join(addon_path,'resources','senderliste.txt')
+    url = 'https://raw.githubusercontent.com/adiko01/plugin.audio.ilovemusic/Daten/Senderlisten/Senderliste-Stabel.txt'
+    r = requests.get(url, allow_redirects=True)
+    open(senderliste, 'wb').write(r.content)
+
+sender_update()
 datei = open(senderliste,'r')
 for zeile in datei:
     sender2 = zeile.split(',')
-    add_item(sender2[0],{'title':sender2[1]},os.path.join(icons_path,sender2[2]))
+    add_item(sender2[0],{'title':sender2[1]},sender2[2])
 
 xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=True, updateListing=False, cacheToDisc=True)
