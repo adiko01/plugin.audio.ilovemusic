8099f9b8371114a7dff536c314ec845244d0bffb
adiko01 <adiko01@adiko01-Fujitsu-M>
2022-06-04 13:39:21 +0200

    Version 3.0.0.2 Die Senderliste kann nun auch ohne Plugin Update aktualisiert werden.

--- plugin.audio.ilovemusic/addon.py
+++ plugin.audio.ilovemusic/addon.py
@@ -4,16 +4,9 @@ import xbmcplugin,xbmcaddon,xbmcgui,os,sys,requests
 
 addon = xbmcaddon.Addon()
 addon_path = addon.getAddonInfo('path').decode('utf-8')
-icons_path = os.path.join(addon_path,'resources','senderlogos')
 senderliste = os.path.join(addon_path,'resources','senderliste.txt')
 xbmcplugin.setContent(handle=int(sys.argv[1]), content='songs')
-				
-def add_item(url,infolabels,img=''):
-    listitem = xbmcgui.ListItem(infolabels['title'])
-    listitem.setInfo('audio',infolabels)
-    listitem.setArt({ 'thumb': img , 'icon' : img })
-    listitem.setProperty('IsPlayable','true')
-    xbmcplugin.addDirectoryItem(int(sys.argv[1]),url,listitem)    
+enable_senderupdate = addon.getSetting('senderupdate')
 
 def sender_update():
     addon_path = addon.getAddonInfo('path').decode('utf-8')
@@ -22,8 +15,17 @@ def sender_update():
     url = 'https://raw.githubusercontent.com/adiko01/plugin.audio.ilovemusic/Daten/Senderlisten/Senderliste-Stabel.txt'
     r = requests.get(url, allow_redirects=True)
     open(senderliste, 'wb').write(r.content)
+				
+def add_item(url,infolabels,img=''):
+    listitem = xbmcgui.ListItem(infolabels['title'])
+    listitem.setInfo('audio',infolabels)
+    listitem.setArt({ 'thumb': img , 'icon' : img })
+    listitem.setProperty('IsPlayable','true')
+    xbmcplugin.addDirectoryItem(int(sys.argv[1]),url,listitem)    
 
-sender_update()
+if enable_senderupdate == 'true':
+    sender_update()
+
 datei = open(senderliste,'r')
 for zeile in datei:
     sender2 = zeile.split(',')
